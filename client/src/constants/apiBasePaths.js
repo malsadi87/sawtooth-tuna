30abb106ea8f9cc374c35b5795c50b28dd8d9a44
Jakob Notland <jakob.notland@ntnu.no>
2022-09-16 15:00:29 +0200

    define all TP API paths

--- client/src/constants/apiBasePaths.js
+++ client/src/constants/apiBasePaths.js
@@ -33,7 +33,49 @@ export const APIBasePath = {
                 getAll: '/sawtooth/tp/trip/',
                 getById: '/sawtooth/tp/trip/getById/:id',
                 createNew: '/sawtooth/tp/trip/addNew'
-            }
+            },
+            haul: {
+                base: '/sawtooth/tp/haul',
+                getAll: '/sawtooth/tp/haul/',
+                getById: '/sawtooth/tp/haul/getById/:id',
+                createNew: '/sawtooth/tp/haul/addNew'
+            },
+            pallet: {
+                base: '/sawtooth/tp/pallet',
+                getAll: '/sawtooth/tp/pallet/',
+                getById: '/sawtooth/tp/pallet/getById/:id',
+                createNew: '/sawtooth/tp/pallet/addNew'
+            },
+            palletEvent: {
+                base: '/sawtooth/tp/palletEvent',
+                getAll: '/sawtooth/tp/palletEvent/',
+                getById: '/sawtooth/tp/palletEvent/getById/:id',
+                createNew: '/sawtooth/tp/palletEvent/addNew'
+            },
+            species: {
+                base: '/sawtooth/tp/species',
+                getAll: '/sawtooth/tp/species/',
+                getById: '/sawtooth/tp/species/getById/:id',
+                createNew: '/sawtooth/tp/species/addNew'
+            },
+            customPackage: {
+                base: '/sawtooth/tp/customPackage',
+                getAll: '/sawtooth/tp/customPackage/',
+                getById: '/sawtooth/tp/customPackage/getById/:id',
+                createNew: '/sawtooth/tp/customPackage/addNew'
+            },
+            company: {
+                base: '/sawtooth/tp/company',
+                getAll: '/sawtooth/tp/company/',
+                getById: '/sawtooth/tp/company/getById/:id',
+                createNew: '/sawtooth/tp/company/addNew'
+            },
+            catchPackage: {
+                base: '/sawtooth/tp/catchPackage',
+                getAll: '/sawtooth/tp/catchPackage/',
+                getById: '/sawtooth/tp/catchPackage/getById/:id',
+                createNew: '/sawtooth/tp/catchPackage/addNew'
+            },
         }
     }
 };
