e12d938af4cc6ceda3225711898135c455b1c661
Jakob Notland <jakob.notland@ntnu.no>
2022-09-29 17:31:16 +0200

    This kind of works. Gets an error that "Jest did not exit one second after the test run has completed." but test run as it should by returning 200 in the test endpoint

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -16,24 +16,24 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api/v1');
+
     await app.init();
 
   });
 
 // unit tests for env file
-describe('env', () => {
   it('should have a node env', () => {
       expect(process.env.NODE_ENV).toBeDefined();
   });
   it('should have a database host', () => {
       expect(process.env.DATABASE_HOST).toBeDefined();
   });
-});
 
   it('/api/v1/test (GET)', () => {
     return request(app.getHttpServer())
       .get('/api/v1/test')
-      .expect(404)
-      //.expect('Hello World!');
+      .expect(200)
+      .expect('Hello world!');
   });
 });
