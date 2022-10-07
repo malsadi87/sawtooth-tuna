7bd374308daed340996907efdedac3cf1207c0bc
Jakob Notland <jakob.notland@ntnu.no>
2022-10-07 18:36:48 +0200

    Test for species

--- backend/test/test.sh
+++ backend/test/test.sh
@@ -6,3 +6,4 @@ npm run test:e2e:tp:pallet || echo 'TEST FAILED: Pallet' &&
 npm run test:e2e:tp:catch-package || echo 'TEST FAILED: CatchPackage' &&
 npm run test:e2e:tp:company || echo 'TEST FAILED: Company' &&
 npm run test:e2e:tp:pallet-event || echo 'TEST FAILED: Pallet-event'
+npm run test:e2e:tp:species || echo 'TEST FAILED: species'
