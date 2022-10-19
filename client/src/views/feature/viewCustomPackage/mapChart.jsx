d6bee87bcb658e8eb5d15f8ea9bb0292234cd7de
Jakob Notland <jakob.notland@ntnu.no>
2022-10-19 14:59:54 +0200

    Checkpoint, ugly and redundant code.

--- client/src/views/feature/viewCustomPackage/mapChart.jsx
+++ client/src/views/feature/viewCustomPackage/mapChart.jsx
@@ -4,7 +4,8 @@ import {
   Geographies,
   Geography,
   ZoomableGroup,
-  Marker
+  Marker,
+  Line
 } from "react-simple-maps";
 
 const geoUrl =
@@ -15,6 +16,86 @@ const MapChart = (props) => {
   useEffect(() => {
     console.log(props)
   }, [props]);
+
+  const flightDestinations = [
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [55.271, 25.205], city: "Dubai" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [-96.797, 32.777], city: "Dallas" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [-123.121, 49.283], city: "Vancouver" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [-43.173, -22.907], city: "Rio De Janiero" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [18.424, -33.925], city: "Cape Town" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [34.782, 32.085], city: "Tel Aviv" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [30.361, 59.931], city: "St petersburg" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [103.82, 1.352], city: "Singapore" },
+    },
+    {
+      from: { coord: [-0.128, 51.507], city: "London" },
+      to: { coord: [-99.133, 19.433], city: "Mexico City" },
+    },
+  ];
+
+  const lines = (flightDestinations) => {
+    return (
+      flightDestinations.map((route) => (
+      <>
+        <Line
+          key={route.to.city}
+          from={route.from.coord}
+          to={route.to.coord}
+          stroke="yellow"
+          strokeWidth={1}
+          strokeLinecap="round"
+        />
+        <Marker coordinates={route.to.coord}>
+          <circle r={2} fill="yellow" />
+        </Marker>
+      </>
+    ))
+    )
+  }
+
+  const linesMarkers = (key, label, coordinatesFrom, coordinatesTo=null) => {
+    return (
+      coordinatesTo ? <>
+        <Line
+          key={key}
+          from={coordinatesFrom}
+          to={coordinatesTo}
+          stroke="purple"
+          strokeWidth={1}
+          strokeLinecap="round"
+        />
+        <Marker coordinates={coordinatesFrom}>
+          <circle r={2.5} fill="yellow" />
+        </Marker>
+      </> :
+        <Marker coordinates={coordinatesFrom}>
+          <circle r={2.5} fill="yellow" />
+        </Marker>
+    )
+  }
 
   return (
     <div>
@@ -27,6 +108,52 @@ const MapChart = (props) => {
               ))
             }
           </Geographies>
+          {props.haulResult ?
+            props.haulResult.map(({ haulId, haulPosition, haulLatitude, haulLongitude }, index) => {
+              return index + 1 < props.haulResult.length ? 
+              linesMarkers(
+                haulId, 
+                haulPosition, 
+                [haulLongitude, haulLatitude], 
+                [ 
+                  props.haulResult[index+1].haulLongitude,
+                  props.haulResult[index+1].haulLatitude
+                ]
+              ) :
+              linesMarkers(
+                haulId, 
+                haulPosition, 
+                [haulLongitude, haulLatitude]
+              )
+            }) : ''}
+
+            {props.palletEventResult ?
+            props.palletEventResult.map(({ palletEventId, location }, index) => {
+              const coordinatesFrom = JSON.parse(location)
+              const palletEventLatitude = coordinatesFrom.latitude
+              const palletEventLongitude = coordinatesFrom.longitude
+              let coordinatesTo = null
+              index + 1 < props.palletEventResult.length ? 
+              coordinatesTo = JSON.parse(props.palletEventResult[index+1].location)
+              : coordinatesTo = null
+              console.log("CoordinatesTo", coordinatesTo)
+              return index + 1 < props.palletEventResult.length ? 
+              linesMarkers(
+                palletEventId, 
+                'palletEvent', 
+                [palletEventLongitude, palletEventLatitude], 
+                [ 
+                  coordinatesTo.longitude,
+                  coordinatesTo.latitude
+                ]
+              ) :
+              linesMarkers(
+                palletEventId, 
+                'palletEvent', 
+                [palletEventLongitude, palletEventLatitude]
+              )
+            }) : ''}
+
           {props.haulResult ?
             props.haulResult.map(({ haulId, haulPosition, haulLatitude, haulLongitude }) => (
               <Marker key={haulId} coordinates={[
@@ -69,29 +196,34 @@ const MapChart = (props) => {
               </Marker>
             )) : ''}
           {props.palletEventResult ?
-            props.palletEventResult.map(({ palletEventId, location }) => { 
+            props.palletEventResult.map(({ palletEventId, location }) => {
               const coordinates = JSON.parse(location)
               const palletEventLatitude = coordinates.latitude
               const palletEventLongitude = coordinates.longitude
               return (
-              <Marker key={palletEventId} coordinates={[palletEventLongitude, palletEventLatitude]}>
-                <circle
-                  className='markerCircle'
-                  r={2}
-                  fill="#F00"
-                  stroke="#fff"
-                  strokeWidth={0.1}
-                />
-                <text
-                  className='markerText'
-                  textAnchor="middle"
-                  y={5}
-                  style={{ fontFamily: "system-ui", fontSize: 6, fill: "#5D5A6D" }}
-                >
-                  palletEvent
-                </text>
-              </Marker>
-            )}) : ''}
+                <Marker key={palletEventId} coordinates={[palletEventLongitude, palletEventLatitude]}>
+                  <circle
+                    className='markerCircle'
+                    r={2}
+                    fill="#F00"
+                    stroke="#fff"
+                    strokeWidth={0.1}
+                  />
+                  <text
+                    className='markerText'
+                    textAnchor="middle"
+                    y={5}
+                    style={{ fontFamily: "system-ui", fontSize: 6, fill: "#5D5A6D" }}
+                  >
+                    palletEvent
+                  </text>
+                </Marker>
+              )
+            }) : ''}
+          {lines(flightDestinations)}
+          <Marker coordinates={[-0.128, 51.507]}>
+            <circle r={2} fill="yellow" />
+          </Marker>
         </ZoomableGroup>
       </ComposableMap>
     </div>
