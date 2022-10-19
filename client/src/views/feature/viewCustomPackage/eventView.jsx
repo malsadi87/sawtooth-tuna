503020834c22eab5f5f87a6d4eb0a0f5785c6915
Jakob Notland <jakob.notland@ntnu.no>
2022-10-19 18:46:58 +0200

    Prepare eventView plots for tilt and shock. Need proper values and validation.

--- client/src/views/feature/viewCustomPackage/eventView.jsx
+++ client/src/views/feature/viewCustomPackage/eventView.jsx
@@ -8,38 +8,95 @@ const EventView = (props) => {
     console.log(props)
   }, [props]);
 
-  let [x,y] = props.palletEventResult ? props.palletEventResult.reduce(([x,y], {eventTime,temperature})=>{
+  let [x, temperatureY, shockY, tiltY] = props.palletEventResult ? props.palletEventResult.reduce((
+    [x, temperatureY, shockY, tiltY],
+    { eventTime, temperature, shock, tilt }) => {
     x.push(eventTime);
     // It is not clear how the format of temperature should be.
-    y.push(temperature.replace(/\D/g, ''));
-    return [x,y];
-  }, [[],[]]) : [[0], [0]]
+    temperatureY.push(temperature.replace(/\D/g, ''));
+    
+    // TODO: Shock and Tilt json object needs strict definition in the entity description.
+    shockY.push(shock.replace(/\D/g, '')); //(JSON.parse(shock).acceleration * JSON.parse(shock).duration)
+
+    tiltY.push(tilt.replace(/\D/g, '')); //(JSON.parse(shock).acceleration * JSON.parse(shock).duration)
+
+    return [x, temperatureY, shockY, tiltY];
+  }, [[], [], [], []]) : [[0], [0], [0], [0]]
 
   return (
-    <div>
-      {props.palletEventResult ? 
-      <Plot
-        layout={{
-          xaxis: {
-            title: {
-              text: "Time",
-            }},
-          yaxis: {
-            title: {
-              text: "Temprature",
-            }}
+    props.palletEventResult ?
+      <>
+        <Plot
+          layout={{
+            xaxis: {
+              title: {
+                text: "Time",
+              }
+            },
+            yaxis: {
+              title: {
+                text: "Temprature",
+              }
+            }
           }}
-        data={[
-          {
-            x: x,
-            y: y,
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: { color: 'red' },
-          },
-        ]}
-      /> : ''}
-    </div>
+          data={[
+            {
+              x: x,
+              y: temperatureY,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'red' },
+            },
+          ]}
+        />
+        <Plot
+          layout={{
+            xaxis: {
+              title: {
+                text: "Time",
+              }
+            },
+            yaxis: {
+              title: {
+                text: "Shock",
+              }
+            }
+          }}
+          data={[
+            {
+              x: x,
+              y: shockY,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'red' },
+            },
+          ]}
+        />
+        <Plot
+          layout={{
+            xaxis: {
+              title: {
+                text: "Time",
+              }
+            },
+            yaxis: {
+              title: {
+                text: "Tilt",
+              }
+            }
+          }}
+          data={[
+            {
+              x: x,
+              y: tiltY,
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'red' },
+            },
+          ]}
+        />
+      </>
+      : <></>
   )
 }
 
