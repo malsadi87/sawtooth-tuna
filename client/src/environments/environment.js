faed8ee455dd2c9b35e94ba90d2cc0de545f2e8b
Amjad Hossain <nayan@Amjad.localdomain>
2022-11-05 02:18:50 +0100

    bug fixing

--- client/src/environments/environment.js
+++ client/src/environments/environment.js
@@ -1,7 +1,14 @@
-export const environment = {
+import { environment as prodenvironment } from "./environment.prod"
+
+export var environment = {
     production: false,
     endPointUrl: 'http://localhost:3001/api/v1',
     cacheTimeInMinute: 1,
     fakeBackEnd: false,
     enableAuth: true
+}
+
+if (process?.env?.NODE_ENV == 'production') {
+    console.log('Production environment found');
+    environment = prodenvironment;
 }
