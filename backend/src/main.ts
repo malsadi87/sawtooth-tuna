e625196ce2b18558be34746f0951af86a7fb1365
Amjad Hossain <nayan@Amjad.localdomain>
2022-11-05 02:22:59 +0100

    changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,14 +5,14 @@ import { AppModule } from './app/app.module';
 import { getProjectConfig } from './app/utility/methods/helper.methods';
 
 async function bootstrap() {
-  const port = getProjectConfig('server').port;
+  const serverConfig = getProjectConfig('server');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors({ origin: ['http://localhost:3000'] });
-  await app.listen(port);
+  app.enableCors({ origin: serverConfig.cors.split(';') });
+  await app.listen(serverConfig.port);
 
   Logger.log(`Current ENVIRONMENT IS ----> ${process.env.NODE_ENV}`);
-  Logger.log(`Server is running on port ${port}`, "Bootstrap");
+  Logger.log(`Server is running on port ${serverConfig.port}`, "Bootstrap");
 }
 
 bootstrap();
