c8c3a8b03b0ead2224033d18979dca030fc9b87b
Amjad Hossain <nayanchowdhury92@gmail.com>
2022-11-26 19:06:52 +0100

    production build and config files error has been fixed

--- client/src/environments/environment.js
+++ client/src/environments/environment.js
@@ -1,5 +1,5 @@
-export var environment = {
-    production: false,
+export const environment = {
+    production: true,
     endPointUrl: 'http://localhost:3001/api/v1',
     cacheTimeInMinute: 1,
     fakeBackEnd: false,
--- client/src/environments/environment.prod.js
+++ client/src/environments/environment.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    endPointUrl: 'https://16.171.28.229:3001/api/v1',
+    endPointUrl: 'http://16.171.28.229:3001/api/v1',
     cacheTimeInMinute: 1,
     fakeBackEnd: false,
     enableAuth: true
