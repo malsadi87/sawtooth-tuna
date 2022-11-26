46f6d2b2b4d5a7f4deee2afaff7370cc13fd3b41
root <root@Amjad.localdomain>
2022-11-27 00:21:04 +0100

    prod env bug fix

--- backend/src/app/utility/filter/api-exception-filter.filter.ts
+++ backend/src/app/utility/filter/api-exception-filter.filter.ts
@@ -21,15 +21,14 @@ export class ApiExceptionFilter extends BaseExceptionFilter {
     const responseObj = {
       statusCode: status,
       timestamp: new Date().toISOString(),
-      path: request.url
+      path: request.url,
+      error: ''
     };
 
-    if (this.NODE_ENV && ['dev', 'test'].includes(this.NODE_ENV)) {
-      if (exception instanceof HttpException) 
-        return super.catch(exception, host);
-      else
-        responseObj['error'] = (exception as Error).message;
-    }
+    if (this.NODE_ENV == 'prod' && status == 500)
+        responseObj.error = (exception as Error).message;
+    else
+      return super.catch(exception, host);
 
     response
       .status(status)
